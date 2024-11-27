f7da70fbecbc6d168e173c965826f0957877f8dd
mayur <mayurbpatil1209@gmail.com>
2024-11-27 17:11:22 +0530

    Push to Gitfirst from Gitfirst

---
+++ src/Practise/PractiseClass.java
@@ -0,0 +1,12 @@
+package Practise;
+
+public class PractiseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I am learning Automation");
+		
+	}
+
+}
